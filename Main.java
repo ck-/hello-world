c062822019eade5a028caf35342e72928697533e
Your Name <you@example.com>
2020-01-19 16:49:03 -0700

    revision three (in master)

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main
 {
-    //this is the chronological revision 1 in master
+    //this is the chronological revision 3 in master
     public static void main(String[] args)
-    {        
+    {
         System.out.println("Hello world");
     }
+    public void methodInMaster()
+    {
+        System.out.println("Method in master");
+    }
 }
+
